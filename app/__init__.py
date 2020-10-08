dbac9bc5a9e388b16ea06b29a480cd454ac4a825
Rachel Friedman <rachelf21@gmail.com>
2020-10-08 09:54:47 -0400

    added mail capabilities, ability to reset password

--- app/__init__.py
+++ app/__init__.py
@@ -28,6 +28,17 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'users.login' #value is function for login, same as url_for
 login_manager.login_message_category = 'info'
 
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+    # app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    # app.config['MAIL_PORT'] = 465
+    # app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
+
+
 from app import routes
 
 from app.users.routes import users
--- app/users/routes.py
+++ app/users/routes.py
@@ -2,11 +2,11 @@ from flask import render_template, url_for, request, redirect, flash, Blueprint
 import secrets
 import os
 from PIL import Image
-from app import app, db, bcrypt
+from app import app, db, bcrypt, mail
 from app.users.forms import RegistrationForm, LoginForm, UpdateAccountForm, RequestResetForm, ResetPasswordForm
 from app.models import Users
 from flask_login import login_user, current_user, logout_user, login_required
-
+from flask_mail import Message
 users = Blueprint('users', __name__)
 
 #%%
@@ -34,7 +34,7 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash('Your account has been created! You are now able to log in', 'success')
-        return redirect(url_for('users.users.login'))
+        return redirect(url_for('users.login'))
     return render_template('register.html', title='Register', form=form)
 
 #%%
@@ -82,10 +82,16 @@ def account():
                            image_file=image_file, form=form)
 #%%
 def send_reset_email(user):
-    pass
+    token = user.get_reset_token()
+    msg = Message('Password Reset Request', sender='noreply@mdy-attendance.com', recipients=[user.email])
 
-
-
+    msg.body = f'''
+    To reset your password, visit the following link:
+    {url_for('users.reset_token', token=token, _external=True)}
+    If you did not make this request then simply ignore this email and no change will be made.
+    '''
+    mail.send(msg)
+        
 #%%
 @users.route("/reset_password", methods=['GET','POST'])
 def reset_request():
@@ -109,4 +115,10 @@ def reset_token(token):
         flash('That is an invalid or expired token', 'warning')
         return redirect(url_for('users.reset_request'))
     form = ResetPasswordForm()
+    if form.validate_on_submit():
+     hashed_pwd = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+     user.password = hashed_pwd
+     db.session.commit()
+     flash('Your password has been updated. You are now able to log in', 'success')
+     return redirect(url_for('users.login'))
     return render_template('reset_token.html', title='Reset Password',form=form)
