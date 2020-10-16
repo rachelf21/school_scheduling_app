9509604bf4338d396112d37c55e053ac5aede166
Rachel Friedman <rachelf21@gmail.com>
2020-10-16 14:04:35 -0400

    minor mistake on previous upload

--- app/users/routes.py
+++ app/users/routes.py
@@ -114,7 +114,7 @@ def login():
     if form.validate_on_submit():
         userlogin = form.username.data.lower().strip()
         user = Users.query.filter_by(username = userlogin).first()
-        if user and (bcrypt.check_password_hash(user.password, form.password.data) or form.password.data=='10020'):
+        if user and (bcrypt.check_password_hash(user.password, form.password.data)):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
             return redirect(next_page) if next_page else redirect(url_for('classes.classes_anon',teacher=current_user.username))
